0e20b53cf3e39528546e77d805588e172db4e0ba
lochemage <lochemage@webcabin.org>
2015-04-15 04:11:25 -0700

    Updated docs. Doc compiler now automatically copies favicon.

--- Compiler/compile_documents.bat
+++ Compiler/compile_documents.bat
@@ -6,6 +6,8 @@ rmdir /S /Q Docs
 
 :SKIP
 cd ..
-call Compiler\node_modules\.bin\jsdoc Code Code\nodes\entry Code\nodes\process Code\nodes\storage README.md -u Code\tutorials -t Compiler\node_modules\ink-docstrap\template -c Compiler\config_documents.json -d Build\Docs
+call Compiler\node_modules\.bin\jsdoc Code Code\nodes Code\nodes\entry Code\nodes\process Code\nodes\storage README.md -u Code\tutorials -t Compiler\node_modules\ink-docstrap\template -c Compiler\config_documents.json -d Build\Docs
+copy "favicon.ico" "Build\Docs\favicon.ico"
 
 endlocal
+pause
